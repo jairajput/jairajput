b5f016fff3e6382d036e4c27023cc4472cd40d24
Jai <80166407+jairajput@users.noreply.github.com>
2021-07-10 12:26:58 +0530

    Create buttons.dart

---
+++ buttons.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+// creating Stateless Wideget for buttons
+class MyButton extends StatelessWidget {
+	
+// declaring variables
+final color;
+final textColor;
+final String buttonText;
+final buttontapped;
+
+//Constructor
+MyButton({this.color, this.textColor, required this.buttonText, this.buttontapped});
+
+@override
+Widget build(BuildContext context) {
+	return GestureDetector(
+	onTap: buttontapped,
+	child: Padding(
+		padding: const EdgeInsets.all(0.2),
+		child: ClipRRect(
+		// borderRadius: BorderRadius.circular(25),
+		child: Container(
+			color: color,
+			child: Center(
+			child: Text(
+				buttonText,
+				style: TextStyle(
+				color: textColor,
+				fontSize: 35,
+				fontWeight: FontWeight.bold,
+				),
+			),
+			),
+		),
+		),
+	),
+	);
+}
+}
